c1248af7f02235bb8562ba1bb678181cbc42e2e4
Jeremy Rose <nornagon@nornagon.net>
2022-02-14 11:59:08 -0800

    add options to symbolicate

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mkdirp = require('mkdirp')
 
 const symbolFiles = new Map
 
-const cacheDirectory = path.resolve(__dirname, 'cache', 'breakpad_symbols')
+const defaultCacheDirectory = () => path.resolve(__dirname, 'cache', 'breakpad_symbols')
 
 async function fetchSymbol(directory, baseUrl, pdb, id, symbolFileName) {
   const url = `${baseUrl}/${encodeURIComponent(pdb)}/${id}/${encodeURIComponent(symbolFileName)}`
@@ -43,14 +43,15 @@ const SYMBOL_BASE_URLS = [
   'https://symbols.electronjs.org',
 ]
 
-async function getSymbolFile(moduleId, moduleName, force) {
+async function getSymbolFile(moduleId, moduleName, opts) {
+  const { cacheDirectory, forceRefetch } = opts
   const pdb = moduleName.replace(/^\//, '')
   const symbolFileName = pdb.replace(/(\.pdb)?$/, '.sym')
   const symbolPath = path.join(cacheDirectory, pdb, moduleId, symbolFileName)
-  if (fs.existsSync(symbolPath) && !force) {
+  if (fs.existsSync(symbolPath) && !forceRefetch) {
     return breakpad.parse(fs.createReadStream(symbolPath))
   }
-  if (!fs.existsSync(symbolPath) && (!fs.existsSync(path.dirname(symbolPath)) || force)) {
+  if (!fs.existsSync(symbolPath) && (!fs.existsSync(path.dirname(symbolPath)) || forceRefetch)) {
     for (const baseUrl of SYMBOL_BASE_URLS) {
       if (await fetchSymbol(cacheDirectory, baseUrl, pdb, moduleId, symbolFileName))
         return breakpad.parse(fs.createReadStream(symbolPath))
@@ -58,14 +59,14 @@ async function getSymbolFile(moduleId, moduleName, force) {
   }
 }
 
-async function symbolicateFrame(frame) {
+async function symbolicateFrame(frame, opts) {
   // ref: https://source.chromium.org/chromium/chromium/src/+/main:third_party/perfetto/src/trace_processor/export_json.cc;l=1413-1420;drc=4e07ae3dd7097bf13b7fff93547d140ac2c8ca83
   const m = /^0x([0-9a-f]+) - (.+) \[([0-9A-F]+)\]$/.exec(frame)
   if (m) {
     const [, offsetHex, moduleName, moduleId] = m
     const offset = parseInt(offsetHex, 16)
     if (!symbolFiles.has(moduleId)) {
-      symbolFiles.set(moduleId, getSymbolFile(moduleId, moduleName))
+      symbolFiles.set(moduleId, getSymbolFile(moduleId, moduleName, opts))
     }
     const symbols = await symbolFiles.get(moduleId)
     if (symbols) {
@@ -78,18 +79,18 @@ async function symbolicateFrame(frame) {
   return frame
 }
 
-async function symbolicateFrames(framesStr) {
+async function symbolicateFrames(framesStr, opts) {
   const frameLines = framesStr.split(/\n/)
-  return (await Promise.all(frameLines.map(symbolicateFrame))).join('\n')
+  return (await Promise.all(frameLines.map(l => symbolicateFrame(l, opts)))).join('\n')
 }
 
-async function maybeSymbolicate(event) {
+async function maybeSymbolicate(event, opts) {
   if (event.cat === 'disabled-by-default-cpu_profiler' && event.args && event.args.frames) {
     return {
       ...event,
       args: {
         ...event.args,
-        frames: await symbolicateFrames(event.args.frames)
+        frames: await symbolicateFrames(event.args.frames, opts)
       }
     }
   } else {
@@ -97,8 +98,13 @@ async function maybeSymbolicate(event) {
   }
 }
 
-async function symbolicate(trace) {
-  return {...trace, traceEvents: await Promise.all(trace.traceEvents.map(maybeSymbolicate))}
+async function symbolicate(trace, options = {}) {
+  const opts = {
+    cacheDirectory: defaultCacheDirectory(),
+    forceRefetch: false,
+    ...options
+  }
+  return {...trace, traceEvents: await Promise.all(trace.traceEvents.map(e => maybeSymbolicate(e, opts)))}
 }
 
 async function cli(argv) {
